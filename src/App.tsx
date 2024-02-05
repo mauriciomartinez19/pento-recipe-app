4156d73520da40fd88abde5e989550ff5ca17802
Mauricio Martinez <mauricio.martinez@norhart.com>
2024-02-05 18:40:07 -0300

    Add section wrapper to fit design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import "./styles/inputs.css";
 import MealTable from "./components/MealTable";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   return (
     <div className="app">
       <h1 className="title">Pento Receipe App</h1>
-      <MealTable/>
+      <div className="section-wrapper">
+        <SearchBox />
+      </div>
+      <div className="section-wrapper">
+        <MealTable />
+      </div>
     </div>
   );
 }
