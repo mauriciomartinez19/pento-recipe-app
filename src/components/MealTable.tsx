8f5d54798d6312a030124e80cb8d2410c5ec3eb6
Mauricio Martinez <mauricio.martinez@norhart.com>
2024-02-06 23:17:49 -0300

    Add min height to the table

--- src/components/MealTable.tsx
+++ src/components/MealTable.tsx
@@ -10,33 +10,35 @@ interface Props {
 const MealTable = ({ meals }: Props) => {
   const [page, setPage] = useState(1);
   return (
-    <div id="meal-table" className="container">
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Area</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Pagination.getItems(meals, page).map((meal) => {
-            const { id, image, name, area, category } = meal;
-            return (
-              <tr key={id}>
-                <td>{id}</td>
-                <td className="name-cell">
-                  {image && name && <img src={image} alt={name} />}
-                  {name}
-                </td>
-                <td>{area}</td>
-                <td>{category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div id="meal-table">
+      <div  className="wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Area</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Pagination.getItems(meals, page).map((meal) => {
+              const { id, image, name, area, category } = meal;
+              return (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td className="name-cell">
+                    {image && name && <img src={image} alt={name} />}
+                    {name}
+                  </td>
+                  <td>{area}</td>
+                  <td>{category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <div id="pagination">
         {Pagination.getPageNumbers(meals).map((number) => (
           <button key={number} onClick={() => setPage(number)}>
